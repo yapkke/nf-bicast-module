c23ff3ed8a1a1ea55b7010c71839efaa9b32705b
mchan.cs.stanford <mchan.cs.stanford@97864374-f055-11dd-a979-a7afd4e285e1>
2009-03-10 12:06:33 +0000

    Added run-bicast.sh

---
+++ trunk/run-bicast.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+################
+# Run constants 
+################
+bond_name=bond0
+bonding_mac_addr=00:1c:f0:ed:98:5a
+init_interface=ath0
+init_essid=cleanslatewifi2
+num_essids=2
+essids[0]=cleanslatewifi2
+essids[1]=cleanslatewifi3
+num_interfaces=2
+interfaces[0]=ath0
+interfaces[1]=wlan0
+time_between_switches=10
+hold_time=2
+
+num_switches=20
+
+cur_interface=$init_interface
+cur_essid=$init_essid
+next_interface=1
+next_essid=1
+
+#############
+# FUNCTIONS #
+#############
+switch_ap()
+{
+	# Make new association
+	iwconfig ${interfaces[$next_interface]} essid ${essids[$next_essid]}
+	sleep 1
+	
+	# Change sending interface
+	ifenslave -c $bond_name ${interfaces[$next_interface]}
+	
+	# Hold
+	sleep $hold_time
+
+	# Break old association
+	iwconfig ${interfaces[$old_interface]} essid xxxx ap off
+}
+
+########
+# Init #
+########
+
+# Set MAC address of bonding driver
+ifconfig $bond_name down
+ifconfig $bond_name hw ether $bonding_mac_addr
+ifconfig $bond_name up
+ifconfig $bond_name
+cat /proc/net/bonding/$bond_name
+
+# Initialize association and active slave
+iwconfig $init_interface essid $init_essid
+sleep 1
+iwconfig $init_interface
+ifenslave -c $bond_name $init_interface
+
+echo "Press <ENTER> to start."
+read
+
+# Start bicast filtering in Netfilter module
+echo "Starting bicast filtering..."
+./nf-start-bicast
+
+sleep $time_between_switches
+
+cur_inteface=$init_interface
+cur_essid=$init_essid
+
+# Switching
+for i in `seq 1 $num_switches`; do
+	let "next_interface=($cur_interface+1)%$num_interfaces"
+	let "next_essid=($cur_essid+1)%$num_essids"
+
+	echo "Switching from ${essids[$cur_essid]} (${interfaces[$cur_interface]}) to ${essids[$next_essid]} (${interfaces[$next_interface]}), hold time = $hold_time" 
+
+	# Make new association
+	iwconfig ${interfaces[$next_interface]} essid ${essids[$next_essid]}
+	sleep 1
+
+	# Change sending interface
+	ifenslave -c $bond_name ${interfaces[$next_interface]}
+	
+	# Hold
+	sleep $hold_time
+
+	# Send stop message to Nox
+	python bicast_noxmsg.py
+	
+	# Break old association
+	iwconfig ${interfaces[$cur_interface]} essid xxxx ap off
+
+	cur_interface=$next_interface
+	cur_essid=$next_essid
+
+	sleep $time_between_switches
+done
+
+# Stop bicast filtering in Netfilter module
+echo "Stopping bicast filtering..."
+./nf-stop-bicast
+
+echo "Done!"
+
