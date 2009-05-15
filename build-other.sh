4840734f723153bcf710a5c43fcc6c8f4a223d91
mchan.cs.stanford <mchan.cs.stanford@97864374-f055-11dd-a979-a7afd4e285e1>
2009-05-15 00:58:47 +0000

    xx

---
+++ build-other.sh
@@ -0,0 +1,6 @@
+gcc -o short-sleep short-sleep.c
+gcc -o nf-start-bicast nf-start-bicast.c
+gcc -o nf-stop-bicast nf-stop-bicast.c
+gcc -o nf-control nf-control.c
+gcc -o change-active-slave change-active-slave.c -I/usr/src/linux/include
+
